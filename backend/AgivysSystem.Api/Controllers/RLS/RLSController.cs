e54c16d89806aadf08a54fea1c6bc8a30ba0efde
joe-blanca-git <joebblanca@gmail.com>
2026-07-30 22:40:31 -0300

    fix: ajuste role 2

--- backend/AgivysSystem.Api/Controllers/RLS/RLSController.cs
+++ backend/AgivysSystem.Api/Controllers/RLS/RLSController.cs
@@ -14,7 +14,7 @@ namespace AgiVysSystem.Api.Controllers.RLS;
 [ApiVersion("1.0")]
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiController]
-[Authorize(Roles = "dev,admin")]
+[Authorize(Roles = "Dev,Admin")]
 public class RLSController : ControllerBase
 {
     private readonly UserManager<User> _userManager;
